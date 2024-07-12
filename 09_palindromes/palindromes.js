69fdabfef801fc4828446be81c592504318f23f0
Sairamreddy8 <challasairamreddy@gmail.com>
2024-07-12 23:11:22 +0530

    09 Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanString = string
+    .toLowerCase()
+    .split('')
+    .filter((character)=> alphanumerical.includes(character))
+    .join('');
+
+    let a = cleanString.split('').reverse().join('');
+
+    return a == cleanString;
 };
 
 // Do not edit below this line
