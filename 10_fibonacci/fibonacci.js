203acc0bb343186e83fe4500b67298b584b71364
Sairamreddy8 <challasairamreddy@gmail.com>
2024-07-12 23:21:06 +0530

    10 Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+if(n==0) return 0;
+if(n==1) return 1;
+let required = fibonacci(n-1) + fibonacci(n-2);
+return required;
 };
 
 // Do not edit below this line
