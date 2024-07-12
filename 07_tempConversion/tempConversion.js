20ef49581e227511770967b69ea57d0d0e61e4e6
Sairamreddy8 <challasairamreddy@gmail.com>
2024-07-12 22:35:04 +0530

    07 Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp-32)*5/9;
+  celsiusTemp = Math.round(celsiusTemp*10)/10;
+  return celsiusTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = (9*celsiusTemp/5) + 32;
+  fahrenheitTemp = Math.round(fahrenheitTemp*10)/10;
+  return fahrenheitTemp
 };
 
 // Do not edit below this line
